ab37b421ce8a3ae391e30d8935e121b20d39746b
NicCOConnor <nic.oconnor@gmail.com>
2013-08-16 23:49:06 -0400

    Some string stuff added
    ways to Concatinate and Escape as well as Constants

--- p003rubystrings.rb
+++ p003rubystrings.rb
@@ -1,1 +1,19 @@
 #p003rubystrings.rb
+=begin
+  Ruby Strings
+  In Ruby, strings are mutable
+=end
+puts "Hello World"
+# Can use " or ' for strings but ' is more efficient
+puts 'Hello World'
+#String Concatination
+puts 'I Like' + 'Ruby'
+#Escape Sequences
+puts 'It\'s my Ruby'
+#New Here Display's the string 3 times
+puts 'Hello' * 3
+# defining a constant 
+# More on Constants later, here
+#http://rubylearning.com/satishtalim/ruby_names.html
+PI = 3.1416
+puts PI  
