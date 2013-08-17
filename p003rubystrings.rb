9eb7368772403ca7d13eac3755a3c4c8c6eb99f4
NicCOConnor <nic.oconnor@gmail.com>
2013-08-17 12:23:42 -0400

    More fun with strings, Multiline

--- p003rubystrings.rb
+++ p003rubystrings.rb
@@ -18,6 +18,33 @@ puts 'Hello' * 3
 PI = 3.1416
 puts PI  
 
-# Running a command through system
-system("dir")
+#defining a local variable
+my_string = 'I Love My City, Pune'
+puts my_string
+=begin
+  Conversions
+  .to_i, .to_f, .to_s
+=end
+var1 = 5;
+var2 = '2'
+puts var1 + var2.to_i
+#<<appending to a string
+a = 'hello '
+a<<'world.
+I Love this world...'
 
+puts a
+=begin
+  << Marks the start of the string literal and 
+  is followed by a delimeter of your choice
+  The String literal then starts from the next 
+  new line and finishes when the delimeter is 
+  repeated again on a line on it's own. This is known as
+  Here document syntax
+=end
+a = <<END_STR
+  This is the string
+  and a second line 
+END_STR
+
+puts a
