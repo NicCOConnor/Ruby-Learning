adec2a9139da9414e4c245f140466ddce152bc4c
NicCOConnor <nic.oconnor@gmail.com>
2013-08-17 12:39:18 -0400

    added p00tdt file something about classes

---
+++ p007dt.rb
@@ -0,0 +1,13 @@
+#p007dt.rb
+# Ruby is dynamic
+x = 7       #integer
+x = "house" #string
+x = 7.5     #real
+
+#In ruby everything you manipulate is an object
+'I Love Ruby'.length
+
+#Messing with classes, Specifically Float
+puts Float::DIG
+
+puts Float::MAX
