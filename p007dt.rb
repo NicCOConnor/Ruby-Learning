c54852efed8c2f498414e13c72b038efe65b96fa
NicCOConnor <nic.oconnor@gmail.com>
2013-08-17 19:26:15 -0400

    Merged PeterCooper

--- p007dt.rb
+++ p007dt.rb
@@ -8,3 +8,17 @@ rice_on_square = 1
    puts "On square #{square + 1} are #{rice_on_square} grains(s)"
    rice_on_square *= 2
 end
+
+#examples of type dynamics
+s = 'hello'
+puts s.class
+
+puts 'I am in class = ' + self.class.to_s
+puts 'I am an object = ' + self.to_s
+print 'The object methods are = '
+puts self.private_methods.sort
+
+#one more example
+5.times { puts "Mice!\n"} #More on blocks later
+  "Elephants Like Peanuts".length
+
