bb70be4f23ec222237b6cc231c3b0b1fb56072a9
NicCOConnor <nic.oconnor@gmail.com>
2013-08-16 23:12:07 -0400

    Added RubyNumbers Script

---
+++ p002rubynumbers.rb
@@ -0,0 +1,17 @@
+#p002rubynumbers.rb
+=begin
+  Ruby Numbers
+  Usual Operators:
+  + Addition
+  - Subtraction
+  * Multiplication
+  / Division
+=end
+
+puts 1+2
+puts 2*3
+#integer division
+#When you do atithmatic with integers, you'll get integer answers
+puts 3/2
+puts 10-11
+puts 1.5/2.6
