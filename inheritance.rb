3f634de971e83c698d7d29e81b24daf9440775dd
NicCOConnor <nic.oconnor@gmail.com>
2013-08-20 20:39:41 -0400

    Changed Inheritance to a different class

--- inheritance.rb
+++ inheritance.rb
@@ -1,59 +1,22 @@
 #Demonstrating class inheritance
-class Mammal
-  def breathe
-    puts 'inhale and exhale'
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def to_s
+    "(#@breed, #@name)"
   end
 end
 
-#Cat Class
-class Cat < Mammal
-  def speak
-    puts 'Meow'
+class Lab < Dog
+  def initialize(breed, name)
+    super(breed)
+    @name = name  
   end
 end
 
-#Dog Class
-class Dog < Mammal
-  def speak
-    puts 'Rough Rough'
-  end
-end
-
-cheeto = Cat.new()
-rylie = Cat.new()
-kia = Dog.new()
-
-cheeto.breathe()
-cheeto.speak()
-
-rylie.breathe()
-rylie.speak
-
-kia.breathe()
-kia.speak
-
-class Bird
-  def preen
-    puts "I am cleaning my feathers."
-  end
-  def fly
-    puts "I am flying"
-  end
-end
-
-class Penguin < Bird
-  def fly
-    puts "Sorry. I'd rather swim"
-  end
-end
-
-p = Penguin.new()
-p.preen()
-p.fly()
-
-b = Bird.new()
-b.preen()
-b.fly()
+puts Lab.new("Labrador", "Benzy").to_s()
 
 
 
