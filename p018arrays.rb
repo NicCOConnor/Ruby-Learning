507303fe172292e2c3531467cf644bf3b208dcb5
NicCOConnor <nic.oconnor@gmail.com>
2013-08-20 23:33:21 -0400

    Added arrays
    Half filled need to finish code

---
+++ p018arrays.rb
@@ -0,0 +1,21 @@
+# p018arrays.rb
+#Arrays
+
+#empty Array
+var1 = []
+
+#Array index starts from 0
+puts var1[0]
+
+# An Array holding a single number
+var2 = [5]
+puts var2[0]
+
+#An Array holding two strings
+var3 = ['Hello', 'Goodnight']
+puts var3[0]
+puts var3[1]
+
+flavour = 'mango'
+
+    
