ab6bd804717e227fc08f4e1ef560c5eb8658e1a8
Nicholas O'Connor <nic.oconnor@gmail.com>
2013-08-21 09:14:45 -0400

    added p019
    More on arrays and iterating them, Multiple assignments.

---
+++ p019mtdarray.rb
@@ -0,0 +1,17 @@
+#p019mtdarray.rb
+# if you give return multiple parameters,
+# the method returns them in an array
+# The times method of the Integer class iterates block num times,
+#passing in values from zero to num-1
+
+def mtdarray
+  10.times do |num|
+    square = num * num
+    return num, square if num > 5
+  end
+end
+
+#using parallel assignment to collect the return value 
+num, square = mtdarray
+puts num
+puts square
