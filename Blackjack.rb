d6dc0593452d8f0e94dd189a91c9ee462fccd333
NicCOConnor <nic.oconnor@gmail.com>
2013-08-22 07:35:24 -0400

    Stopping point

--- Blackjack.rb
+++ Blackjack.rb
@@ -1,25 +1,26 @@
 #This module will attempt to simulate a blackjack game through class inheritance
 module Blackjack
+  SUITS = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+  VALUES = %w"A 1 2 3 4 5 6 7 8 9 10 J Q K"
+  
   class Card
     @suit = nil
     @value = nil
-    SUITS = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
-    VALUES = %w"A 1 2 3 4 5 6 7 8 9 10 J Q K"
     def initialize(suit, value)
-    @suit = {SUITS.include?(suit) ? suit : raise Invalid "That is not a valid card suit"}
-    @value = {VALUES.include?(value) ? value : raise Invalid "That is not a valid card value"}
+    @suit = suit unless SUITS.include?(suit)
+    @value = value unless VALUES.include?(value)
+    [@suit:@value]
     end
   end
-  #collection of cards
   
+  #collection of cards
   class Deck
     #Desk variable
     @d = []
-      
     def initialize()
       SUITS.each do |s|
-        VALUE.each do |v|
-          d += card.new(s,v)
+        VALUES.each do |v|
+          @d << Card.new(s,v)
         end
       end
     end
