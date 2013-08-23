a41c9eee615a0ff450c26668562aee5d8f94d8f8
NicCOConnor <nic.oconnor@gmail.com>
2013-08-22 22:35:54 -0400

    Added a Cat class
    A bit more example code with more dogs a couple cats and a lot of animal
    noises.

--- classes.rb
+++ classes.rb
@@ -16,10 +16,34 @@ class Dog # Must be uppercase first letter
   end
 end
 
+class Cat
+  #Method to Set the Name
+   def set_name(aName)
+     @myname = aName
+   end
+   #Method to Get the Name
+   def get_name
+     return @myname
+   end
+   #Method to speak
+   def talk
+     return 'meow!' 
+   end
+end
 mydog = Dog.new
 yourdog = Dog.new
+anotherdog = Dog.new
+cat1 = Cat.new()
+cat2 = Cat.new()
+cat1.set_name('Cheeto')
+cat2.set_name('Rylie')
+mydog.set_name('Kia')
+yourdog.set_name('Sasha')
 
-mydog.set_name('slut')
-yourdog.set_name('whore')
-puts mydog.get_name()
-puts mydog.talk()
+
+puts "the pets I own are:"
+puts cat1.get_name() + " he says " + cat1.talk
+puts cat2.get_name() + " he says " + cat2.talk
+puts mydog.get_name() + " he says " + mydog.talk
+puts yourdog.get_name() + " she says " + yourdog.talk
+puts anotherdog.get_name() + " she says " + anotherdog.talk
