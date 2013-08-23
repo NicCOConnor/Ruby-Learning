3d8b36017d4c7b7f75582c717868771b3c263de6
NicCOConnor <nic.oconnor@gmail.com>
2013-08-22 22:06:58 -0400

    Modified calctax to accept an input

--- 4calctax.rb
+++ 4calctax.rb
@@ -1,4 +1,6 @@
-subtotal = 100.00
-taxrate = 0.175
+taxrate = 0.045
+print "Enter price (ex tax): "
+s = gets
+subtotal = s.to_f
 tax = subtotal * taxrate
 puts "Tax on $#{subtotal} is $#{tax}, so grand total is $#{subtotal+tax}"
