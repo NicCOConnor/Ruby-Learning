37203071d9ea458a39efca8a70ba7b461a9cd77c
NicCOConnor <nic.oconnor@gmail.com>
2013-09-03 22:11:57 -0400

    Done with arrays.
    Pretty good primer.

--- Arrays.rb
+++ Arrays.rb
@@ -51,7 +51,60 @@ end
 for (a,b,c,d) in multiarr
   print("a=#{a}, b=#{b}, c=#{c}, d=#{d}\n")
 end
-     
- 
+
+#Iterates through values of the array of unknown row length
+for row in multiarr
+  for item in row
+    printf("%s ",item)
+  end
+  printf("\n")
+end
+
+#Indexing into arrays
+arr = ['h','e','l','l','o','w','o','r','l','d']
+
+print(arr[0,5])
+print(arr[-5,5])
+print(arr[0..4])
+print(arr[-5..-1])
   
+#Copying Array
+arr1 = %w[h e l l o w o r l d]
+arr2 = arr1 #arr2 is now the same as arr1. Change arr1 and arr2 changes too
+arr3 = arr1.clone() #arr3 is copied and not changed when arr1 is changed. 
+arr1[1] = 'a'
+puts
+print arr1
+print arr2
+print arr3
+
+#Testing Arrays for Equality
+
+test1 = [0,10,20]
+test2 = [0,20,20]
+puts
+puts test1 <=> test2 #-1 if left is less then right 0 if both are equal +1 if right is greater then left.
+
+#Sorting Arrays
+arr1.sort{
+  |a,b|
+  a.to_s <=> b.to_s
+}
+print arr1.sort()
+
+class MyArray < Array
+  include Comparable
+  
+  def <=> (anotherArray)
+    self.length() <=> anotherArray.length()
+  end
+end
+puts #Need a newline for readablity
+
+myarr1 = MyArray.new([0,1,2,3,5])
+myarr2 = MyArray.new([1,2,3,4,5,6])
+
+puts myarr1 <=> myarr2
+
+
   
