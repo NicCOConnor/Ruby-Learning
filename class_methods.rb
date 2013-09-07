c1c0935abe2555247f1ac6c72aefb9340d3b39b8
NicCOConnor <nic.oconnor@gmail.com>
2013-09-07 17:45:01 -0400

    added class_methods.rb difference between class methods and variables

---
+++ class_methods.rb
@@ -0,0 +1,21 @@
+class MyClass
+  def MyClass.classMethod
+    puts("This is a class method")
+  end
+  def instanceMethod
+    puts("This is an instance method")
+  end
+end
+
+MyClass.classMethod
+ob = MyClass.new
+ob.instanceMethod
+
+#Class variables
+class Thing
+  @@num_things = 0
+
+  def initialize(aName, aDescription)
+    @@num_things += 1
+  end
+end
