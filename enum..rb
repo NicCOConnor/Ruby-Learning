fc627d6472cafa4678d7368384998e18d2bd4d17
NicCOConnor <nic.oconnor@gmail.com>
2013-09-07 13:28:42 -0400

    enum class put in and done

--- enum..rb
+++ enum..rb
@@ -1,32 +1,20 @@
-arr = [1,2,3,4,5]
-y = arr.collect{|i|}
-z = arr.collect{|i| i * i}
+#implementing Enumerable
+class MyCollection
+  include Enumerable
 
-p(arr.include?(3))
-p(arr.include?(6))
-p(arr.min)
-p(arr.max)
+  def initialize(someItems)
+    @items = someItems
+  end
 
-#Custom Comparison
-str_arr = "%w[one two three four five six seven]"
-h = {'one' =>'for sorrow',
-    'two'=>'for joy',
-    'three'=>'for a girl',
-    'four'=>'for a boy',
-    'five'=>'for sorrow',
-    'six'=>'for gold',
-    'seven'=>'for a secret never to be told'}
-puts('==================== h.keys ===================')
+  def each
+    @items.each{|i|
+      yield(i)
+    }
+  end
+end
 
-p(h.min{|a,b| a[0].length <=> b[0].length})
-p(h.max{|a,b| a[0].length <=> b[0].length})
+things = MyCollection.new(['x', 'yz', 'defgh', 'ij', 'klmno'])
+p(things.min)
+p(things.max)
+p(things.collect{|i| i.upcase})
 
-puts('==================== h.values =================')
-
-p(h.min{|a,b| a[1].length <=> b[1].length})
-p(h.max{|a,b| a[1].length <=> b[1].length})
-
-puts('==================== custom order =============')
-
-p( h.min{|a,b| str_arr.index(a[0]) <=> str_arr.index(b[0])})
-p( h.max{|a,b| str_arr.index(a[0]) <=> str_arr.index(b[0])})
