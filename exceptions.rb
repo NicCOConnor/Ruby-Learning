db5d8143475ec06290254be61860be04830930cb
NicCOConnor <nic.oconnor@gmail.com>
2013-09-08 11:36:09 -0400

    added exceptions.rb Changed Exception Class to handle more exceptions

--- exceptions.rb
+++ exceptions.rb
@@ -19,12 +19,17 @@ end
 def calc(val1, val2)
   begin
     result = val1 / val2
+  rescue TypeError, NoMethodError => e
+    puts(e.class)
+    puts(e)
+    puts("One of the values is not a number!")
+    result = nil
   rescue Exception => e
     puts(e.class)
     puts(e)
     result = nil
   end
-  result = nil
+  return result
 end
 
 puts(calc(10,20))
