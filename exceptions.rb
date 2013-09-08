fa1ed8cf60065502c412e1a18a9397e6401dcc56
NicCOConnor <nic.oconnor@gmail.com>
2013-09-08 11:26:21 -0400

    added exceptions.rb bout to change up the calc class to handle more exceptions

---
+++ exceptions.rb
@@ -0,0 +1,33 @@
+#Simple example of an exception
+begin
+  x = 1/0
+rescue Exception
+  x = 0
+  puts($!.class)
+  puts($!)
+end
+
+#Instead of using non-descriptive variables you can assign the exception a variable like this
+begin
+  x = 1/0
+rescue Exception => ex
+  puts(ex.class)
+  puts(ex)
+end
+
+#Type Exception errors. Such as trying to divide by a string
+def calc(val1, val2)
+  begin
+    result = val1 / val2
+  rescue Exception => e
+    puts(e.class)
+    puts(e)
+    result = nil
+  end
+  result = nil
+end
+
+puts(calc(10,20))
+puts(calc(100,0))
+puts(calc('hello', 20))
+puts(calc(20, 'hello'))
